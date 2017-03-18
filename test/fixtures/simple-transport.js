91e09d0515d0bfdbcd78bf993bad1cece77ad569
indexzero <charlie.robbins@gmail.com>
2017-03-18 18:47:01 -0400

    [test] Considerable test coverage.

---
+++ test/fixtures/simple-transport.js
@@ -0,0 +1,39 @@
+const TransportStream = require('../../');
+const util = require('util');
+
+/**
+ * !!! HERE BE DRAGONS !!!
+ *
+ * Constructor function for the SimpleTransport which we use for
+ * testing purposes. It breaks all the established rules and
+ * conventions for testing edge cases. You SHOULD NOT use this
+ * as an example for how to write a custom `winston` Transport.
+ *
+ * @param  {Object} opts Configuration for this instance
+ */
+const SimpleTransport = module.exports = function (opts) {
+  TransportStream.call(this, opts);
+
+  //
+  // Remark: we literally accept levels for TESTING PURPOSES only.
+  // In `winston` these levels will always inherit from the Logger
+  // we are piped to.
+  //
+  this.levels = opts.levels;
+  this.stream = opts.stream;
+  this.streams = opts.streams;
+};
+
+util.inherits(SimpleTransport, TransportStream);
+
+/**
+ * @param  {Info} info Winston log information
+ * @param  {Function} callback Continuation to respond to when complete
+ * @return {[type]}
+ */
+SimpleTransport.prototype.log = function (info, callback) {
+  const stream = (this.streams && this.streams[info.level]) || this.stream;
+  stream.write(JSON.stringify(info));
+  this.emit('logged');
+  callback();
+};
