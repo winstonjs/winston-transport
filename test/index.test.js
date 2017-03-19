808fc1be4809bf5c3cd44832e8acc6373479b989
indexzero <charlie.robbins@gmail.com>
2017-03-19 14:38:47 -0400

    [api test] Begin to move common test functionality over to `abstract-winston-transport` for re-use and recycle.

--- test/index.test.js
+++ test/index.test.js
@@ -1,54 +1,9 @@
-
 const assume = require('assume');
 const stream = require('stream');
 const TransportStream = require('../');
 const Parent = require('./fixtures/parent');
 const SimpleTransport = require('./fixtures/simple-transport');
-
-//
-// Returns a function which logs a specified amount
-// of times before calling the provided callback
-//
-function logFor(count, done) {
-  const infos = [];
-  return function log(info, next) {
-    infos.push(info);
-    next();
-    if (!--count) { return done && done(null, infos); }
-    if (count < 0) {
-      throw new Error('Invoked more log messages than requested');
-    }
-  };
-}
-
-//
-// Helper function for generating a set of messages
-// one per level.
-//
-function levelAndMessage (level) {
-  return {
-    message: `Testing message for level: ${level}`,
-    level
-  };
-}
-
-//
-// Inspects two arrays
-//
-function inspectLoggedResults(actual, expected) {
-  const len = actual.length > expected.length
-    ? actual.length
-    : expected.length;
-
-  console.log(`Length: { actual: ${actual.length}, expected: ${expected.length}`);
-
-  for (let i = 0; i < len; i++) {
-    if (actual[i] || expected[i]) {
-      console.log(`actual[${i}]: %j`, actual[i]);
-      console.log(`expected[${i}]: %j`, expected[i]);
-    }
-  }
-}
+const { logFor, levelAndMessage } = require('abstract-winston-transport/utils');
 
 //
 // Order of Levels used in these tests.
