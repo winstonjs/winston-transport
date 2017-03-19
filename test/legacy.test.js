8b75cc179f249f2459e878665fc76b27d22201bd
indexzero <charlie.robbins@gmail.com>
2017-03-19 14:59:21 -0400

    [test fix] Begin testing `LegacyTransportStream`.

--- test/legacy.test.js
+++ test/legacy.test.js
@@ -1,2 +1,34 @@
 'use strict';
 
+const assume = require('assume');
+const stream = require('stream');
+const LegacyTransportStream = require('../legacy');
+const Parent = require('./fixtures/parent');
+const LegacyTransport = require('./fixtures/legacy-transport');
+const { logFor, levelAndMessage } = require('abstract-winston-transport/utils');
+
+describe('LegacyTransportStream', function () {
+  it('should have the appropriate methods defined', function () {
+    const legacy = new LegacyTransport();
+    const transport = new LegacyTransportStream({ transport: legacy });
+    assume(transport).instanceof(stream.Writable);
+    assume(transport._write).is.a('function');
+    assume(transport.log).equals(undefined);
+  });
+
+  it('should error with no transport', function () {
+    assume(function () {
+      const transport = new LegacyTransportStream();
+      assume(transport).instanceof(stream.Writable);
+    }).throws(/Invalid transport, must be an object with a log method./);
+  });
+
+  it('should error with invalid transport', function () {
+    assume(function () {
+      const transport = new LegacyTransportStream();
+      assume(transport).instanceof(stream.Writable);
+    }).throws(/Invalid transport, must be an object with a log method./);
+  });
+
+
+});
