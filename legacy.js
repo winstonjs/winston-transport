8928bf714ba434d929d0df6cafaeb70d13a06377
indexzero <charlie.robbins@gmail.com>
2018-05-30 17:07:15 +0200

    [api] Allow for prototypal inheritance from Transport and LegacyTransport.

--- legacy.js
+++ legacy.js
@@ -1,115 +1,119 @@
 'use strict';
 
-const TransportStream = require('./');
+const util = require('util');
 const { LEVEL } = require('triple-beam');
+const TransportStream = require('./');
 
-module.exports = class LegacyTransportStream extends TransportStream {
-  /**
-   * Constructor function for the LegacyTransportStream. This is an internal
-   * wrapper `winston >= 3` uses to wrap older transports implementing
-   * log(level, message, meta).
-   * @param {Object} options - Options for this TransportStream instance.
-   * @param {Transpot} options.transport - winston@2 or older Transport to wrap.
-   */
-  constructor(options = {}) {
-    super(options);
-    if (!options.transport || typeof options.transport.log !== 'function') {
-      throw new Error('Invalid transport, must be an object with a log method.');
-    }
+/**
+ * Constructor function for the LegacyTransportStream. This is an internal
+ * wrapper `winston >= 3` uses to wrap older transports implementing
+ * log(level, message, meta).
+ * @param {Object} options - Options for this TransportStream instance.
+ * @param {Transpot} options.transport - winston@2 or older Transport to wrap.
+ */
 
-    this.transport = options.transport;
-    this.level = this.level || options.transport.level;
-    this.handleExceptions = this.handleExceptions || options.transport.handleExceptions;
-
-    // Display our deprecation notice.
-    this._deprecated();
-
-    // Properly bubble up errors from the transport to the
-    // LegacyTransportStream instance, but only once no matter how many times
-    // this transport is shared.
-    function transportError(err) {
-      this.emit('error', err, this.transport);
-    }
-
-    if (!this.transport.__winstonError) {
-      this.transport.__winstonError = transportError.bind(this);
-      this.transport.on('error', this.transport.__winstonError);
-    }
+const LegacyTransportStream = module.exports = function LegacyTransportStream(options = {}) {
+  TransportStream.call(this, options);
+  if (!options.transport || typeof options.transport.log !== 'function') {
+    throw new Error('Invalid transport, must be an object with a log method.');
   }
 
+  this.transport = options.transport;
+  this.level = this.level || options.transport.level;
+  this.handleExceptions = this.handleExceptions || options.transport.handleExceptions;
 
-  /**
-   * Writes the info object to our transport instance.
-   * @param {mixed} info - TODO: add param description.
-   * @param {mixed} enc - TODO: add param description.
-   * @param {function} callback - TODO: add param description.
-   * @returns {undefined}
-   * @private
-   */
-  _write(info, enc, callback) {
-    if (this.silent || (info.exception === true && !this.handleExceptions)) {
-      return callback(null);
-    }
+  // Display our deprecation notice.
+  this._deprecated();
 
-    // Remark: This has to be handled in the base transport now because we
-    // cannot conditionally write to our pipe targets as stream.
-    if (!this.level || this.levels[this.level] >= this.levels[info[LEVEL]]) {
-      this.transport.log(info[LEVEL], info.message, info, this._nop);
-    }
-
-    callback(null);
+  // Properly bubble up errors from the transport to the
+  // LegacyTransportStream instance, but only once no matter how many times
+  // this transport is shared.
+  function transportError(err) {
+    this.emit('error', err, this.transport);
   }
 
-  /**
-   * Writes the batch of info objects (i.e. "object chunks") to our transport
-   * instance after performing any necessary filtering.
-   * @param {mixed} chunks - TODO: add params description.
-   * @param {function} callback - TODO: add params description.
-   * @returns {mixed} - TODO: add returns description.
-   * @private
-   */
-  _writev(chunks, callback) {
-    for (let i = 0; i < chunks.length; i++) {
-      if (this._accept(chunks[i])) {
-        this.transport.log(
-          chunks[i].chunk[LEVEL],
-          chunks[i].chunk.message,
-          chunks[i].chunk,
-          this._nop
-        );
-        chunks[i].callback();
-      }
-    }
+  if (!this.transport.__winstonError) {
+    this.transport.__winstonError = transportError.bind(this);
+    this.transport.on('error', this.transport.__winstonError);
+  }
+};
 
+/*
+ * Inherit from TransportStream using Node.js built-ins
+ */
+util.inherits(LegacyTransportStream, TransportStream);
+
+/**
+ * Writes the info object to our transport instance.
+ * @param {mixed} info - TODO: add param description.
+ * @param {mixed} enc - TODO: add param description.
+ * @param {function} callback - TODO: add param description.
+ * @returns {undefined}
+ * @private
+ */
+LegacyTransportStream.prototype._write = function _write(info, enc, callback) {
+  if (this.silent || (info.exception === true && !this.handleExceptions)) {
     return callback(null);
   }
 
-  /**
-   * Displays a deprecation notice. Defined as a function so it can be
-   * overriden in tests.
-   * @returns {undefined}
-   */
-  _deprecated() {
-    // eslint-disable-next-line no-console
-    console.error([
-      `${this.transport.name} is a legacy winston transport. Consider upgrading: `,
-      '- Upgrade docs: https://github.com/winstonjs/winston/blob/master/UPGRADE-3.0.md'
-    ].join('\n'));
+  // Remark: This has to be handled in the base transport now because we
+  // cannot conditionally write to our pipe targets as stream.
+  if (!this.level || this.levels[this.level] >= this.levels[info[LEVEL]]) {
+    this.transport.log(info[LEVEL], info.message, info, this._nop);
   }
 
-  /**
-   * Clean up error handling state on the legacy transport associated
-   * with this instance.
-   * @returns {undefined}
-   */
-  close() {
-    if (this.transport.close) {
-      this.transport.close();
-    }
+  callback(null);
+};
 
-    if (this.transport.__winstonError) {
-      this.transport.removeListener('error', this.transport.__winstonError);
-      this.transport.__winstonError = null;
+/**
+ * Writes the batch of info objects (i.e. "object chunks") to our transport
+ * instance after performing any necessary filtering.
+ * @param {mixed} chunks - TODO: add params description.
+ * @param {function} callback - TODO: add params description.
+ * @returns {mixed} - TODO: add returns description.
+ * @private
+ */
+LegacyTransportStream.prototype._writev = function _writev(chunks, callback) {
+  for (let i = 0; i < chunks.length; i++) {
+    if (this._accept(chunks[i])) {
+      this.transport.log(
+        chunks[i].chunk[LEVEL],
+        chunks[i].chunk.message,
+        chunks[i].chunk,
+        this._nop
+      );
+      chunks[i].callback();
     }
+  }
+
+  return callback(null);
+};
+
+/**
+ * Displays a deprecation notice. Defined as a function so it can be
+ * overriden in tests.
+ * @returns {undefined}
+ */
+LegacyTransportStream.prototype._deprecated = function _deprecated() {
+  // eslint-disable-next-line no-console
+  console.error([
+    `${this.transport.name} is a legacy winston transport. Consider upgrading: `,
+    '- Upgrade docs: https://github.com/winstonjs/winston/blob/master/UPGRADE-3.0.md'
+  ].join('\n'));
+};
+
+/**
+ * Clean up error handling state on the legacy transport associated
+ * with this instance.
+ * @returns {undefined}
+ */
+LegacyTransportStream.prototype.close = function close() {
+  if (this.transport.close) {
+    this.transport.close();
+  }
+
+  if (this.transport.__winstonError) {
+    this.transport.removeListener('error', this.transport.__winstonError);
+    this.transport.__winstonError = null;
   }
 };
